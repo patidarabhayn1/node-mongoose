eae75761a97127a5e9a17a554fa4bff4af3a859a
Abhay Patidar <patidarabhayn1@gmail.com>
2020-11-28 21:10:53 +0530

    Schema inside Schema

--- index.js
+++ index.js
@@ -17,10 +17,25 @@ connect.then((db) => {
         .then((dish) => {
             console.log(dish);
 
-            return Dishes.find({});
+            return Dishes.findByIdAndUpdate(dish._id, {
+                $set: {description: 'Updated Test'}
+            },{
+                new: true
+            }).exec();
         })
-        .then((dishes) => {
-            console.log(dishes);
+        .then((dish) => {
+            console.log(dish);
+
+            dish.comments.push({
+                rating: 5,
+                comment: 'I\'m getting a sinking feeling!',
+                author : 'Abhay Patidar'
+            });
+
+            return dish.save();
+        })
+        .then((dish) =>{
+            console.log(dish);
 
             return Dishes.deleteMany({});
         })
